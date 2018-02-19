346bcafe0de2300143d7233ead912948b8fa121b
DK\pk <pk@sitecore.net>
2018-02-19 16:28:15 +0200

    Introduced a new <initialize> processor to switch ServicePointManager.SecurityProtocol to Tls12

---
+++ src/Sitecore.Support.207852/InitializeServicePointManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using Sitecore.Pipelines;
+
+namespace Sitecore.Support.Pipelines.Loader
+{
+    public class InitializeServicePointManager
+    {
+        public void Process(PipelineArgs args)
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+        }
+    }
+}
---
+++ src/Sitecore.Support.207852/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.207852")]
+[assembly: AssemblyProduct("Sitecore.Support.207852")]
+[assembly: ComVisible(false)]
